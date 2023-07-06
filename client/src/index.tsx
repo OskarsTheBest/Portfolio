7419443263c54396e5870f8536e3fe6eddaa82de
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-07-06 11:04:08 +0300

    initialize the project

---
+++ client/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Main from './Main';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <h1>yo</h1>
+  </React.StrictMode>
+);
+
