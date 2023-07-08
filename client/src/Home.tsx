878606e94cef603c8c7df164a96c2c96f9c21f53
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-07-08 14:01:29 +0300

    Project setting up

---
+++ client/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>yo</h1>
+    </div>
+  );
+}
+
+export default Home;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Main from './Main';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Home";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
 root.render(
   <React.StrictMode>
-    <h1>yo</h1>
-  </React.StrictMode>
+    <Home />
+  </React.StrictMode>,
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
