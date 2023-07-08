33d14b0d72a367096f328edeb44338959ad4d7ce
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-07-09 00:41:24 +0300

    added unlock screen and some other features

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,11 +1,55 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./Home.css";
+import LocalTime from "./components/LocalTime";
 
 function Home() {
+  const [isUnlocked, setIsUnlocked] = useState(false);
+  const [initialMouseY, setInitialMouseY] = useState<number | null>(null);
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (e.button === 0) { // Only trigger when left mouse button is pressed
+      setInitialMouseY(e.clientY);
+    }
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (initialMouseY === null) return;
+
+    const currentMouseY = e.clientY;
+    const deltaY = currentMouseY - initialMouseY;
+
+    if (deltaY > 50) {
+      setIsUnlocked(true);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setInitialMouseY(null);
+  };
+
   return (
-    <div className="App">
-      <h1>yo</h1>
+    <div className="PhoneFront flex items-center justify-center h-screen">
+      {!isUnlocked ? (
+        <div
+          className="box-border h-5/6 w-3/12 p-4 border-8 rounded-md border-black flex flex-col justify-center items-center cursor-pointer"
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
+          {/* Unlock Screen */}
+          <div className="Time absolute top-40 ">
+          <LocalTime></LocalTime>
+          </div>
+          <div className="swipe-indicator">Drag mouse down to unlock</div>
+          
+        </div>
+      ) : (
+        <div className="box-border h-5/6 w-3/12 p-4 border-8 rounded-md border-black">
+          {/* Content after unlocking */}
+          <div>About Me</div>
+          <div>Project</div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ client/src/components/LocalTime.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+function LocalTime() {
+  const [time, setTime] = useState(getFormattedTime());
+  const [date, setDate] = useState(getFormattedDate());
+
+  function getFormattedTime() {
+    const date = new Date();
+    const hour = date.getHours().toString().padStart(2, "0");
+    const minute = date.getMinutes().toString().padStart(2, "0");
+    return `${hour}:${minute}`;
+  }
+
+  function getFormattedDate() {
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "long" });
+    return `${day}${getOrdinalSuffix(day)} ${month}`;
+  }
+
+  function getOrdinalSuffix(day: number) {
+    const suffixes = ["th", "st", "nd", "rd"];
+    const relevantDigits = day % 100;
+    const ordinalIndex = (relevantDigits - 20) % 10;
+    const suffix = suffixes[ordinalIndex] || suffixes[relevantDigits] || suffixes[0];
+    return suffix;
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(getFormattedTime());
+      setDate(getFormattedDate());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="font-extrabold text-4xl ">{time}</div>
+      <div className="font-extralight justify">{date}</div>
+      </div>
+
+  );
+}
+
+export default LocalTime;
