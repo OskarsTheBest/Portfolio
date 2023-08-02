f9c072a71ee7475c8a4c736d37e08c9d1873d605
OskarsTheBest <oskarsbest@gmail.com>
2023-08-02 20:23:59 +0200

    Cleaned up the code BY ALOT

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,15 +1,9 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import LocalTime from "./components/LocalTime";
 import MouseWrap from "./components/MouseWrap";
-import $ from "jquery";
-import Phoneright from "./Phoneright";
-import Phoneleft from "./Phoneleft";
-import Phoneback from "./Phoneback";
 import Phonescreen from "./PhoneInside/Phonescreen";
-
-let moving = false;
-let page = 0;
+import $ from "jquery";
 
 function Home() {
   const [backgroundImage, setBackgroundImage] = useState<string>(
@@ -18,94 +12,47 @@ function Home() {
   const [isLoading, setIsLoading] = useState(true);
   const [isUnlocked, setIsUnlocked] = useState(false);
   const [initialMouseY, setInitialMouseY] = useState<number | null>(null);
-  const [displayPhoneright, setDisplayPhoneright] = useState(false);
-  const [displayPhoneleft, setDisplayPhoneleft] = useState(false);
-  const [displayPhoneback, setDisplayPhoneback] = useState(false);
+  let moving = false;
+  let page = 0;
+  
 
-  const handleRightButtonClick = useCallback(() => {
-    if (moving === false) {
-      moving = true;
-      $(".load-right").addClass("active");
-      setTimeout(function () {
-        $(".load-right").one("animationiteration webkitAnimationIteration mozAnimationIteration oAnimationIteration oanimationiteration", function () {
-          $(".load-right").removeClass("active");
-          $(".load-right").one("transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd", function () {
-            if (page === 0) {
-              setDisplayPhoneright(true);
-              setDisplayPhoneleft(false);
-              setDisplayPhoneback(false);
-              page++;
-            } else if (page === 1) {
-              setDisplayPhoneback(true);
-              setDisplayPhoneright(false);
-              setDisplayPhoneleft(false);
-              page++;
-            } else {
-              setDisplayPhoneright(true);
-              setDisplayPhoneleft(false);
-              setDisplayPhoneback(false);
-            }
-            moving = false;
+
+  //btn animation
+  useEffect(() => {
+    const animationIteration =
+      "animationiteration webkitAnimationIteration mozAnimationIteration oAnimationIteration oanimationiteration";
+    const transitionEnd =
+      "transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd";
+  
+    const handleLoadClick = (direction: 'right' | 'left') => {
+      if (moving === false) {
+        moving = true;
+        $(`.load-${direction}`).addClass("active");
+        setTimeout(function () {
+          $(`.load-${direction}`).one(animationIteration, function () {
+            $(`.load-${direction}`).removeClass("active");
+            $(`.load-${direction}`).one(transitionEnd, function () {
+              if (direction === 'right') {
+                page++;
+              } else if (direction === 'left') {
+                page--;
+              }
+              moving = false;
+            });
           });
-        });
-      }, 2000);
-    }
-  }, [page]);
-
-  const handleLeftButtonClick = useCallback(() => {
-    if (moving === false) {
-      moving = true;
-      $(".load-left").addClass("active");
-      setTimeout(function () {
-        $(".load-left").one("animationiteration webkitAnimationIteration mozAnimationIteration oAnimationIteration oanimationiteration", function () {
-          $(".load-left").removeClass("active");
-          $(".load-left").one("transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd", function () {
-            if (page === 0) {
-              setDisplayPhoneleft(true);
-              setDisplayPhoneback(false);
-              setDisplayPhoneright(false);
-              page--;
-            } else if (page === 1) {
-              setDisplayPhoneright(false);
-              setDisplayPhoneleft(false);
-              setDisplayPhoneback(false);
-              page--;
-            } else {
-              setDisplayPhoneback(true);
-              setDisplayPhoneright(false);
-              setDisplayPhoneleft(false);
-            }
-            moving = false;
-          });
-        });
-      }, 2000);
-    }
-  }, [page]);
-
-  useEffect(() => {
-    const image = new Image();
-    image.onload = () => {
-      setIsLoading(false);
+        }, 2000);
+      }
     };
-    image.src = backgroundImage;
-  }, [backgroundImage]);
-
-  useEffect(() => {
-    $(".load-right").on("click", handleRightButtonClick);
-
-    return () => {
-      $(".load-right").off("click", handleRightButtonClick);
-    };
-  }, [handleRightButtonClick]);
-
-  useEffect(() => {
-    $(".load-left").on("click", handleLeftButtonClick);
-
-    return () => {
-      $(".load-left").off("click", handleLeftButtonClick);
-    };
-  }, [handleLeftButtonClick]);
-
+  
+    $(".load-right").on("click", function () {
+      handleLoadClick('right');
+    });
+  
+    $(".load-left").on("click", function () {
+      handleLoadClick('left');
+    });
+  }, []);
+  
   const handleMouseDown = (e: React.MouseEvent) => {
     if (e.button === 0) {
       // Only trigger when the left mouse button is pressed
@@ -127,11 +74,18 @@ function Home() {
   const handleMouseUp = () => {
     setInitialMouseY(null);
   };
-    //TODO:
-    //FIX THE BUTTONS
+
+  useEffect(() => {
+    const image = new Image();
+    image.onload = () => {
+      setIsLoading(false);
+    };
+    image.src = backgroundImage;
+  }, [backgroundImage]);
+
   return (
     <div className="PhoneFront flex items-center justify-center h-screen">
-      {page === 0 && !isUnlocked ? (
+      {!isUnlocked ? (
         <>
           {isLoading ? (
             <div className="box-border h-5/6 w-3/12 p-4 border-8 rounded-md border-black bg-white flex flex-col justify-center items-center cursor-pointer">
@@ -157,30 +111,22 @@ function Home() {
             </div>
           )}
         </>
-      ) : page === 0 && isUnlocked ? (
+      ) : (
         <>
           <div className="box-border h-5/6 w-3/12 p-4 border-8 rounded-md border-black bg-white">
             {/* Content after unlocking */}
             <Phonescreen></Phonescreen>
           </div>
-          {displayPhoneright && <Phoneright />}
-        </>
-      ) : page === 1 ? (
-        <>
-          {displayPhoneleft && <Phoneleft />}
-          {displayPhoneback && <Phoneback />}
-        </>
-      ) : (
-        <>
-          {displayPhoneright && <Phoneright />}
-        </>
+          </>
       )}
-      <button onClick={handleRightButtonClick}>
-          <span className="load-right load-more"></span>
-      </button>
-      <button onClick={handleLeftButtonClick}>
-          <span className="load-left load-more"></span>
-      </button>
+          {/* Buttons for further actions */}
+          <button>
+            <span className="load-right load-more"></span>
+          </button>
+          <button>
+            <span className="load-left load-more"></span>
+          </button>
+
     </div>
   );
 }
