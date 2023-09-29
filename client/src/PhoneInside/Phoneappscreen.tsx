a9b5c020434f4eca670ee52c036b5ec61b6f2b1d
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-09-29 18:57:22 +0300

    added app btns, updated search

--- client/src/PhoneInside/Phoneappscreen.tsx
+++ client/src/PhoneInside/Phoneappscreen.tsx
@@ -19,9 +19,29 @@ function Phoneappscreen() {
       <div className='my-6'>
         <LocalTime />
       </div>
-      <div className='max-w-sm mx-auto rounded-xl p-2 bg-gray-400'>
+      <div className='max-w-sm mx-auto rounded-xl p-2 text-white bg-gray-400 brightness-75 hover:brightness-100'>
+      <img className='h-6 w-6 float-right' src={search} alt='Search'></img>
         Search This Phone
-        <img className='ml-56 h-6 w-6 ' src={search} alt='Search'></img>
+      </div>
+      <div className="grid grid-cols-2 gap-6 box-content auto-cols-max border-black py-12">
+        <div className="bg-blue-500 p-4 rounded-lg text-white text-center hover:bg-blue-600 transform hover:scale-105 transition-transform h-28">
+          <p>App 1</p>
+        </div>
+        <div className="bg-green-500 p-4 rounded-lg text-white text-center hover:bg-green-600 transform hover:scale-105 transition-transform h-28">
+          <p>App 2</p>
+        </div>
+        <div className="bg-yellow-500 p-4 rounded-lg text-white text-center hover:bg-yellow-600 transform hover:scale-105 transition-transform h-28">
+          <p>App 3</p>
+        </div>
+        <div className="bg-red-500 p-4 rounded-lg text-white text-center hover:bg-red-600 transform hover:scale-105 transition-transform h-28">
+          <p>App 4</p>
+        </div>
+        <div className="bg-purple-500 p-4 rounded-lg text-white text-center hover:bg-purple-600 transform hover:scale-105 transition-transform h-28">
+          <p>App 5</p>
+        </div>
+        <div className="bg-pink-500 p-4 rounded-lg text-white text-center hover:bg-pink-600 transform hover:scale-105 transition-transform h-28">
+          <p>App 6</p>
+        </div>
       </div>
     </div>
   )
